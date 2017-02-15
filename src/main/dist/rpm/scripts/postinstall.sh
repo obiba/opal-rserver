f8f1e588e38eac056dfa553a6a5f4096c0dce40f
Yannick Marcon <yannick.marcon@obiba.org>
2017-02-15 12:44:48 +0100

    Install tidyverse packages

--- src/main/dist/rpm/scripts/postinstall.sh
+++ src/main/dist/rpm/scripts/postinstall.sh
@@ -7,7 +7,7 @@ set -e
 case "$1" in
     1)
 
-      Rscript -e "install.packages(c('opal', 'ggplot2', 'haven', 'rmarkdown'), repos=c('http://cran.rstudio.com', 'http://cran.obiba.org'), dependencies=TRUE, lib='/usr/local/lib/R/site-library')"
+      Rscript -e "install.packages(c('opal', 'tidyverse', 'knitr', 'rmarkdown'), repos=c('http://cran.rstudio.com', 'http://cran.obiba.org'), dependencies=TRUE, lib='/usr/local/lib/R/site-library')"
 
       Rscript -e "if (R.Version()\$major >= 3) { install.packages('opaladdons', repos=c('http://cran.rstudio.com', 'http://cran.obiba.org'), dependencies=TRUE, lib='/usr/local/lib/R/site-library') }"
 
