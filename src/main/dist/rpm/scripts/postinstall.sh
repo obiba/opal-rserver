6536a9480fa6b61d590815a22c9c687ee62d7202
Yannick Marcon <yannick.marcon@obiba.org>
2020-06-19 09:00:03 +0200

    #8 opal, opaladdons removed, BiocManager added

--- src/main/dist/rpm/scripts/postinstall.sh
+++ src/main/dist/rpm/scripts/postinstall.sh
@@ -7,9 +7,7 @@ set -e
 case "$1" in
     [1-2])
 
-      Rscript -e "install.packages(c('opal', 'opalr', 'tidyverse', 'knitr', 'rmarkdown'), repos=c('https://cloud.r-project.org', 'https://cran.obiba.org'), dependencies=TRUE, lib='/usr/share/R/library')"
-
-      Rscript -e "if (R.Version()\$major >= 3) { install.packages('opaladdons', repos=c('https://cloud.r-project.org', 'https://cran.obiba.org'), dependencies=TRUE, lib='/usr/share/R/library') }"
+      Rscript -e "install.packages(c('opalr', 'tidyverse', 'knitr', 'rmarkdown', 'BiocManager'), repos=c('https://cloud.r-project.org', 'https://cran.obiba.org'), dependencies=TRUE, lib='/usr/share/R/library')"
 
     ;;
 
